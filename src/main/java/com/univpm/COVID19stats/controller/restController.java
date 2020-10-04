0d8d83a07c5a533488977425065eb5023c3cfda1
alesimattia <alesimattia@gmail.com>
2020-10-04 22:04:42 +0200

    Homepage
    Creata una "homepage" con un riepilogo dello stato attuale

--- src/main/java/com/univpm/COVID19stats/controller/restController.java
+++ src/main/java/com/univpm/COVID19stats/controller/restController.java
@@ -1,9 +1,5 @@
 package com.univpm.COVID19stats.controller;
 
-import java.net.http.HttpResponse;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +8,6 @@ import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.univpm.COVID19stats.model.Country;
 
 @RestController
@@ -32,11 +27,13 @@ public class restController {
 	}
 	
 	@RequestMapping(method=RequestMethod.GET, value="/", produces="application/json" )
-	public String home() throws JsonMappingException, JsonProcessingException {
-		/*final String uri ="https://api.covid19api.com/summary";
-		ObjectMapper mapper = new ObjectMapper();
-	    JSONPObject resume = mapper.readValue(uri, String.class);
-		return resume.getJSONObject(0);*/
-		return "Pagina iniziale";
+	public String home() {
+		final String uri ="https://api.covid19api.com/summary";
+		
+		RestTemplate restTemplate = new RestTemplate();
+	    String ris = restTemplate.getForObject(uri, String.class);
+	    
+	    //tronca parte delle informazioni nel json
+	    return "{"+ ris.substring(14, ris.indexOf("}")) + "}}";
 	}
 }
