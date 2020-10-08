3b5de7b283e75896c8a1b0c80d981e87b5d8b85f
marcoincipini <48022522+marcoincipini@users.noreply.github.com>
2020-10-08 15:17:00 +0200

    Struttura prog

---
+++ src/main/java/com/univpm/COVID19stats/model/Response.java
@@ -0,0 +1,20 @@
+package com.univpm.COVID19stats.model;
+
+public class Response {
+    private Bundle max;
+    private Bundle min;
+
+    public Bundle getMax() {
+		return max;
+	}
+	public void setMax(Bundle max) {
+		this.max = max;
+	}
+	public Bundle getMin() {
+		return min;
+	}
+	public void setMin(Bundle min) {
+		this.min = min;
+	}
+
+}
