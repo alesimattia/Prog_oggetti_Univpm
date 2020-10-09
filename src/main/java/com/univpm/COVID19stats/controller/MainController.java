8ba308bd06426c548962f80f2cbf89fba4a7941a
marcoincipini <48022522+marcoincipini@users.noreply.github.com>
2020-10-09 22:04:49 +0200

    Delimiter fixed
    url errato... le chiamare nonvengono effettuate

--- src/main/java/com/univpm/COVID19stats/controller/MainController.java
+++ src/main/java/com/univpm/COVID19stats/controller/MainController.java
@@ -11,40 +11,43 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.Scanner;
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.regex.*;
 
 @RestController
 public class MainController {
-	
+
 	/*Homepage con dati riassuntivi*/
 	@RequestMapping(method=RequestMethod.GET, value="/", produces="application/json" )
 	public String home() {
 		final String url = "https://api.covid19api.com/summary";
-		
+
 		RestTemplate restTemplate = new RestTemplate();
-	    String ris = restTemplate.getForObject(url, String.class);
-	    
-	    //tronca parte delle informazioni nel json
-	    return "{"+  ris.substring( 14, ris.indexOf("}") )
-	    	 + "},"+ ris.substring( ris.length()-31, ris.length() );
+		String ris = restTemplate.getForObject(url, String.class);
+
+		//tronca parte delle informazioni nel json
+		return "{"+  ris.substring( 14, ris.indexOf("}") )
+		+ "},"+ ris.substring( ris.length()-31, ris.length() );
 	}
-	
-	
+
+
 	@RequestMapping(method=RequestMethod.POST, value="/{categoria}", produces="application/json" )
 	public String Dati(@RequestParam(name="categoria",defaultValue="contagi") String categoria,	@RequestBody String body ) throws JsonProcessingException {
 
 		Scanner in = new Scanner(body);
-	    in.useDelimiter(",");
+		Pattern patt=Pattern.compile("\\},");
+		in.useDelimiter(patt);
 		ObjectMapper obj = new ObjectMapper();
-		Vector<Paese> paesi= new Vector<Paese>();
+		ArrayList<Paese> paesi= new ArrayList<Paese>();
 		Filtro filtro=new Filtro();
 		while(in.hasNext()) {
-			String json=in.next();
+			String json=in.next()+"}";
 			if(json.contains("percentuale")) {
 				try {
 					filtro=obj.readValue(json, Filtro.class);
@@ -60,19 +63,20 @@ public class MainController {
 			}
 		}
 		in.close();
-		
+
 		Response risposta=new Response();
-		
+		RequestGenerator rg = new RequestGenerator();
+		FormatData formatter = new FormatData();
+
 		for(Paese p:paesi) {
-			Vector<Bundle> dato=new Vector<Bundle>();
-			//request gen
-			//Formatdata 
+			ArrayList<Bundle> dato=new ArrayList<Bundle>();
+			dato.addAll(rg.getData (categoria, p.getSlug()));
+			//Formatdata
 			//Filter
 			//Responsegenerator
 		}
-		//risposta tojason
 		//return risposta
 		return categoria;
 	}
-  
+
 }
