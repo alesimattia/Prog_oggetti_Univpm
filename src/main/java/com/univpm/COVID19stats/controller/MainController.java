709fecb478a53dc0f94a5d44e98f360e95f741cb
marcoincipini <48022522+marcoincipini@users.noreply.github.com>
2020-10-17 13:47:09 +0200

    main completo

--- src/main/java/com/univpm/COVID19stats/controller/MainController.java
+++ src/main/java/com/univpm/COVID19stats/controller/MainController.java
@@ -3,7 +3,7 @@ package com.univpm.COVID19stats.controller;
 import com.univpm.COVID19stats.model.Paese;
 
 import com.univpm.COVID19stats.model.Response;
-
+import com.univpm.COVID19stats.model.ResponseStat;
 import com.univpm.COVID19stats.model.Bundle;
 import com.univpm.COVID19stats.model.Filtro;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -45,6 +45,8 @@ public class MainController {
 	@RequestMapping(method=RequestMethod.POST, value="/{categoria}", produces="application/json" )
 	public String Dati(@RequestParam(name="categoria",defaultValue="contagi") String categoria,	@RequestBody String body ) throws JsonProcessingException, ParseException {
 
+		body=body.replaceAll("[\\[\\]]","");
+		body=body.replaceAll(" ","");
 		Scanner in = new Scanner(body);
 		Pattern patt=Pattern.compile("\\},");
 		in.useDelimiter(patt);
@@ -106,6 +108,87 @@ public class MainController {
 
 		String risp ="";
 		for(Response r:risposta)
+			risp+=obj.writeValueAsString(r);
+			
+			//Provato a sostituire Date con stringa direttamente nel json
+			/*int start = risp.lastIndexOf("\"Data\":\"")+1;
+			int end = risp.indexOf("lat:");
+			
+			char[] dateChar = null;
+			risp.getChars(start, end, dateChar, 0);
+			DateFormat format = new SimpleDateFormat("dd-MM-yyyy", Locale.ITALIAN);
+			Date date = format.parse(dateChar.toString());
+			String dateString = date.toGMTString();
+			
+			risp.replace(dateChar.toString(), dateString);*/
+		
+		return risp;
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/{categoria}/{tipostat}", produces="application/json" )
+	public String Dati(@RequestParam(name="categoria",defaultValue="contagi") String categoria, @RequestParam(name="tipostat",defaultValue="contagiorni") String tipostat,	@RequestBody String body ) throws JsonProcessingException, ParseException {
+
+		body=body.replaceAll("[\\[\\]]","");
+		body=body.replaceAll(" ","");
+		Scanner in = new Scanner(body);
+		Pattern patt=Pattern.compile("\\},");
+		in.useDelimiter(patt);
+		
+		ObjectMapper obj = new ObjectMapper();
+		DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.ITALIAN);
+		obj.setDateFormat(dateFormat);
+		
+		//Informazioni contenute nel requestBody
+		ArrayList<Paese> paesi= new ArrayList<Paese>();
+		Filtro filtro = new Filtro();
+		boolean hasFilter = false;
+		
+		/** Converte il requestBody negli oggetti Paese e Filtro */
+		while(in.hasNext()) {
+			String json=in.next()+"}";
+			if(json.contains("percentuale")) {
+				try {
+					filtro = obj.readValue(json, Filtro.class);
+					hasFilter = true;
+				}
+				catch(JsonProcessingException e){
+					e.printStackTrace();
+				}
+			}
+			else {
+				try {
+					paesi.add(obj.readValue(json, Paese.class));
+				}
+				catch(JsonProcessingException e){
+					e.printStackTrace();
+				}
+			}
+		}
+		in.close();
+    
+		RequestGenerator requestGen = new RequestGenerator();
+		FormatData formatter = new FormatData();
+		Filter dataFilter = new Filter();
+		StatsGenerator responseStat = new StatsGenerator();
+		ArrayList<ResponseStat> risposta=new ArrayList<ResponseStat>();
+
+		for(Paese p:paesi) {
+			ArrayList<Bundle> dato=new ArrayList<Bundle>();
+			Response response = new Response();
+			dato.addAll( requestGen.getData (categoria, p.getSlug() ) );
+			
+			if(hasFilter) {
+				formatter.convert(dato, filtro.isPercentuale() );
+				dataFilter.filtra(dato, filtro);
+			}
+			else
+				formatter.convert(dato, hasFilter );
+			
+			risposta.add(responseStat.getStat(dato, tipostat, categoria));
+		}
+
+		String risp ="";
+		for(ResponseStat r:risposta)
 			risp+=obj.writeValueAsString(r);
 			
 			//Provato a sostituire Date con stringa direttamente nel json
